2ce0004e59edfaa29f2f794004f589232f875873
mirvohid04 <shodiyevmirvohid131@gmail.com>
2025-08-21 18:37:31 +0500

    pagelar qushdim faqat ui

---
+++ lib/ui/pages/auth/WinningPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WinningPage extends StatefulWidget {
+  const WinningPage({super.key});
+
+  @override
+  State<WinningPage> createState() => _WinningPageState();
+}
+
+class _WinningPageState extends State<WinningPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
